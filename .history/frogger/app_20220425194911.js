0fe0e9f9ad56b32eac0b8c7958936ed2e3e3bb2c
Woojin <cmshim22@gmail.com>
2022-04-29 22:59:16 +0900

    space

---
+++ .history/frogger/app_20220425194911.js
@@ -0,0 +1,11 @@
+const timeLeftDisplay = document.querySelector("#time-left");
+const resultDisplay = document.querySelector("#result");
+const StartPauseButton = document.querySelector("#start-pause-button");
+const squares = document.querySelectorAll(".grid div");
+
+console.log(squares);
+function moveFrog() {
+  //   console.log("moved");
+}
+
+document.addEventListener("keyup", moveFrog);
---
+++ .history/frogger/app_20220425195042.js
@@ -0,0 +1,13 @@
+const timeLeftDisplay = document.querySelector("#time-left");
+const resultDisplay = document.querySelector("#result");
+const StartPauseButton = document.querySelector("#start-pause-button");
+const squares = document.querySelectorAll(".grid div");
+
+console.log(squares);
+let currentIndex = 0;
+
+function moveFrog() {
+  squares[currentIndex].classList.add("frog");
+}
+
+document.addEventListener("keyup", moveFrog);
